f50b2d31228692a52770d50dd9dd80ad10e0358c
Ghost <karthiknaik435@gmail.com>
2024-10-28 09:31:35 +0530

    Connected to DB

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database');
+});
+
+app.get("/",(req,res)=>{
+    res.send("Homepage");
+})
+
+app.listen(8080, () => { 
+    console.log("App is listening on PORT 8080");
+})
