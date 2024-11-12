9374ddfde51a7d4dbad1e6b7909efecf7dabb82a
Praneet <praneetvkhp@gmail.com>
2024-11-12 08:35:19 +0530

    Added css to jobdetails

--- frontend/src/landing_page/jobs/JobDetails.jsx
+++ frontend/src/landing_page/jobs/JobDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ApplyForm from "./ApplyForm";
+import "./JobDetails.css";
 
 function JobDetails() {
   const { job_id } = useParams();
